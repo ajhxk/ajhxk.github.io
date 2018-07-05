2cc54623ec919a2a5f58542bdb143f203743c8ac
ajhxk <753663797@qq.com>
2018-07-05 22:35:42 +0800

    ''

--- TryMinex/common/i18n.js
+++ TryMinex/common/i18n.js
@@ -1,9 +1,15 @@
 I18n = (function () {
 
     function I18n() {
-
+        this.langage = {};
     }
 
-    I18n.prototype.addLan
+    I18n.prototype.addLanguag = function addLanguag(key, language) {
+        this.langage[key] = language;
+    }
+
+    I18n.prototype.getText = function getText() {
+
+    }
 
 })();
